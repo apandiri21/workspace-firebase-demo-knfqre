5d4e1d0a18a2813d27c4d8ae02957bae63046553
apandiri21 <apandiri@usca.edu>
2020-11-23 12:02:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# workspace-firebase-demo-gltbjv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/workspace-firebase-demo-gltbjv)
---
+++ js/hotel-json.js
@@ -0,0 +1,69 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
+  authDomain: "grocerylist-91956.firebaseapp.com",
+  databaseURL: "https://grocerylist-91956.firebaseio.com",
+  projectId: "grocerylist-91956",
+  storageBucket: "grocerylist-91956.appspot.com",
+  messagingSenderId: "813812426276",
+  appId: "1:813812426276:web:93e5897af12892ff78dab1",
+  measurementId: "G-VZ83BTR72T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+/* object examples 
+var testJson = {};
+testJson["lastname"] = "zhang";
+testJson["location"] = "aiken";
+console.log(testJson);
+*/
+
+// enter data in
+$("input[type='button']").click(function(e) {
+  //get the value of form
+  var inputdata = $("form").serializeArray();
+  console.log(inputdata);
+  var inputJson = {};
+  for (var i = 0; i < inputdata.length; i++) {
+    var name = inputdata[i]["name"];
+    var value = inputdata[i]["value"];
+    //console.log(name + " " + value);
+    inputJson[name] = value;
+  }
+
+  console.log(inputJson);
+  /* save the data to database */
+  firebase
+    .firestore()
+    .collection("hoteldata") // use different name
+    .add(inputJson);
+
+  /* clear the entry */
+  $("form")[0].reset();
+});
+
+/* array example
+const array1 = ['a', 'b', 'c'];
+array1.forEach(element => console.log(element));
+*/
+
+/* read the data from the database */
+
+firebase
+  .firestore()
+  .collection("hoteldata")
+  .onSnapshot(function(querySnapshot) {
+    //console.log(querySnapshot);
+    console.log(querySnapshot.size);
+    querySnapshot.forEach(doc =>{
+      console.log(doc.data());
+    //  var roomtype= doc.data().room;
+    //   if (roomtype =="1 Bedroom Smaller Suite $169"){
+    //     do sth
+    //   console.log("This roomtype");
+    //  }
+   
+
+      
+    });
+
+  });
